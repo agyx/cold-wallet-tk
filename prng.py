59ab6f05f97ee4c94306fb2035873860fdede684
Bertrand Marlier <contact@bmarlier.com>
2024-06-16 15:32:01 +0200

    refonte prng et ajout d'entropie initiale externe en cas de choix non déterministe

--- prng.py
+++ prng.py
@@ -12,13 +12,15 @@ from io import BytesIO
 import bip39
 import optparse
 
+import bip39words
 from hmac_drbg import HmacDrbg
 
 version = "v3.1.0"
 
 # pwcharset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 pwcharset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
-
+unique_id_charset = "abcdefghijklmnopqrstuvwxyz"
+pwdigitset = "0123456789"
 
 class Prng:
     """
@@ -33,6 +35,8 @@ class Prng:
         self.round = 0
         self.timestampRef = 0
         self.impl = HmacDrbg(seed=seed)
+        if not self.deterministic:
+            self.addExternalEntropy()
 
     def addExternalEntropy(self):
         self.add_entropy(os.urandom(64))
@@ -49,6 +53,17 @@ class Prng:
 
     def add_entropy(self, entropy):
         self.impl.add_entropy(entropy)
+
+    def get_random_integer(self, max_value):
+        pow2 = 1
+        nbits = 0
+        while pow2 < max_value:
+            pow2 *= 256
+            nbits += 8
+        while True:
+            value = self.getRandomLong(nbits)
+            if value < max_value:
+                return value
 
     def getRandomLong(self, nBits):
         if nBits % 8 != 0:
@@ -74,29 +89,56 @@ def bytes2long(_bytes):
     return result
 
 
-def long2str(value, mapfn, modulo, length):
+def split_value(value, item_count, item_range):
     work_value = value
-    result = ""
-    for _ in range(length):
-        result += mapfn(work_value % modulo)
-        work_value //= modulo
-        if work_value <= modulo:
+    result = []
+    entropy = 1
+    for i in range(item_count):
+        result.append(work_value % item_range)
+        entropy *= item_range
+        work_value //= item_range
+        if work_value <= item_range:
             raise OverflowError
-    return result
+    bits_entropy = 0
+    while entropy > 1:
+        entropy //= 2
+        bits_entropy += 1
+    return (result, bits_entropy)
 
 
+def long2str(value, charset, length):
+    (arr, entropy) = split_value(value, length, len(charset))
+    result = ""
+    for index in arr:
+        result += charset[index]
+    return f"{result} ({entropy} bits)"
+
+
+def get_words_password(value, nwords=4, uppercase=False, sep=' ', digits=False):
+    (arr, entropy) = split_value(value, nwords, 2048)
+    dico = bip39words.bip39Words["french"].split("\n")
+    result = []
+    count = 1
+    for index in arr:
+        prefix = ""
+        if digits:
+            prefix += f"{count}"
+            count += 1
+        mot = dico[index]
+        if uppercase:
+            mot = mot[0].upper() + mot[1:]
+        result.append(prefix + mot)
+    return f"{sep.join(result)} ({entropy} bits)"
+
+# def show_password(prompt, value, charset):
+
+    
 def genRandomInteractive(prng, lang="english"):
     result = prng.random_bytes(size=64)
     long_result = bytes2long(result)
     hexResult = binascii.hexlify(result)
 
     offset = 0
-
-    def mapPasswordChar(char):
-        return pwcharset[char % len(pwcharset)]
-
-    def mapPasswordDigit(char):
-        return chr(ord('0') + char % 10)
 
     seed128bits = hexResult[offset:offset + 32]
     seed256bits = hexResult[offset:offset + 64]
@@ -111,18 +153,28 @@ def genRandomInteractive(prng, lang="english"):
     print("256 bits: " + seed256bits.decode("utf-8"))
     # print "512 bits: " + hexResult[offset:offset+128]
     print("")
-    print("Password (16 chars)  : %s" % long2str(long_result, mapPasswordChar, len(pwcharset), 16))
-    print("Password (24 chars)  : %s" % long2str(long_result, mapPasswordChar, len(pwcharset), 24))
-    print("Password (32 chars)  : %s" % long2str(long_result, mapPasswordChar, len(pwcharset), 32))
-    print("Password (48 chars)  : %s" % long2str(long_result, mapPasswordChar, len(pwcharset), 48))
-    print("Password (4 digits)  : %s" % long2str(long_result, mapPasswordDigit, 10, 4))
-    print("Password (8 digits)  : %s" % long2str(long_result, mapPasswordDigit, 10, 8))
-    print("Password (16 digits) : %s" % long2str(long_result, mapPasswordDigit, 10, 16))
-    print("Password (32 digits) : %s" % long2str(long_result, mapPasswordDigit, 10, 32))
+    print("Unique ID (6 chars)  : %s" % long2str(long_result, unique_id_charset, 6))
     print("")
+    # print("Password (16 chars)  : %s" % long2str(long_result, pwcharset, 16))
+    print("Password (24 chars)  : %s" % long2str(long_result, pwcharset, 24))
+    print("Password (32 chars)  : %s" % long2str(long_result, pwcharset, 32))
+    print("Password (48 chars)  : %s" % long2str(long_result, pwcharset, 48))
+    print("")
+    print("Pincode (4 digits)  : %s" % long2str(long_result, pwdigitset, 4))
+    print("Pincode (6 digits)  : %s" % long2str(long_result, pwdigitset, 6))
+    print("Pincode (8 digits)  : %s" % long2str(long_result, pwdigitset, 8))
+    # print("Password (16 digits) : %s" % long2str(long_result, "0123456789", 16))
+    # print("Password (32 digits) : %s" % long2str(long_result, "0123456789", 32))
+    print("")
+    # print("Password (4 words)  : %s" % get_words_password(long_result))
+    # print("Password (4 words)  : %s" % get_words_password(long_result, sep=";", uppercase=True, digits=True))
+    # print("")
     print("BIP39 12 words: " + " ".join(bip39Seed12))
     print("BIP39 24 words: " + " ".join(bip39Seed24))
     print("--------------------------------------------------------------------------------")
+
+
+# ref: https://proton.me/blog/what-is-password-entropy
 
 
 if __name__ == "__main__":
@@ -141,6 +193,7 @@ if __name__ == "__main__":
         from genkey import genKey
 
         userdata = getpass.getpass("What's up today? :")
+        prng.addExternalEntropy()
         prng.add_entropy(genKey(userdata, prng.impl.random_bytes(32)))
         genRandomInteractive(prng, options.lang)
 
